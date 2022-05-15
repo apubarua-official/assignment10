c9627c0869117f122329d02c9cbe64c965cff593
Apu Barua <100984721+apubarua-official@users.noreply.github.com>
2022-05-15 21:39:34 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+// 01. Create an email pattern 
+
+// let txt = "apubarua.web@gmail.com";
+// let pattern = /^[a-z0-9_\.]*[a-z]@[a-z]{3,}\.[a-z]{2,5}$/;
+// console.log(pattern.test(txt));
+
+
+// 02. Create a  username pattern
+
+// let txt = "apubarua_24";
+// let pattern = /^[a-zA-Z0-9_@]*$/;
+// console.log(pattern.test(txt));
+
+// 03. Create a Bangladeshi phone number pattern
+
+// let txt = "+8801515297951";
+// let pattern = /^(\+880|01)[0-9]{9,10}$/;
+// console.log(pattern.test(txt));
+
+// 04. Create a password pattern 
+
+// let txt = "@123avV@!~";
+// let pattern = /^[@#$%]*[0-9]*[a-z]{2,}[A-Z]{1}[~@!#$%^&*()]*$/;
+// console.log(pattern.test(txt));
+
+// 05. Create a zipcode pattern
+
+let txt = "1212";
+let pattern = /^[0-9]{4}$/;
+console.log(pattern.test(txt));
+
+
